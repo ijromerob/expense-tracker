a2c2631b10290bc6d75873fb1d5a8eeb82c28494
ijromerob <ijromerob@gmail.com>
2025-03-13 14:14:38 -0600

    working in data validation

--- Validation.java
+++ Validation.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Validation {
 
     float validateFloat(float number){
@@ -6,8 +8,19 @@ public class Validation {
             System.out.println("You entered an invalid number");
             System.out.println("the number should be a positive");
             System.out.print("Please enter a valid number: ");
-            
+            number = Float.parseFloat(scanner.nextLine());
         };
         return number;
+    }
+
+    float validateNumeric(string number){
+        float convertedNumber;
+        while(true){
+            try {
+                convertedNumber = 
+            } catch (Exception e) {
+                // TODO: handle exception
+            }
+        }
     }
 }
