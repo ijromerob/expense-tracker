e2d504e477d752c0dc07e35ae307013ec67f5f9c
ijromerob <ijromerob@gmail.com>
2025-03-14 16:36:11 -0600

    added file manager

--- FileManager.java
+++ FileManager.java
@@ -1,11 +1,24 @@
 import java.io.File;
-
+import java.util.Scanner;
 public class FileManager {
+    File transactionsFile;
+
     void createFile(String fileName) {
+
         try {
-            File newFile = new File(fileName);
+            transactionsFile = new File(fileName);
         } catch (Exception e) {
-            // TODO: handle exception
+
+            System.out.println("There was a problem creating your file");
+        }
+    }
+
+    void loadFile(String fileName) {
+        try {
+            transactionsFile = new File(fileName);
+            Scanner myReader = new Scanner(transactionsFile);
+        } catch (Exception e) {
+            System.out.println("There was a problem reading your file");
         }
     }
 }
