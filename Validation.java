32971141926ad80fd843e8129f1b67a0c19df3a3
ijromerob <ijromerob@gmail.com>
2025-03-13 20:15:41 -0600

    added validateNumber to the validation class

--- Validation.java
+++ Validation.java
@@ -2,25 +2,39 @@ import java.util.Scanner;
 
 public class Validation {
 
-    float validateFloat(float number){
-        Scanner scanner = new Scanner(System.in);
-        while (number <= 0){
-            System.out.println("You entered an invalid number");
-            System.out.println("the number should be a positive");
-            System.out.print("Please enter a valid number: ");
-            number = Float.parseFloat(scanner.nextLine());
-        };
-        return number;
+  float validateFloat(float number) {
+    Scanner scanner = new Scanner(System.in);
+    while (number <= 0) {
+      System.out.println("You entered an invalid number");
+      System.out.println("the number should be a positive");
+      System.out.print("Please enter a valid number: ");
+      number = Float.parseFloat(scanner.nextLine());
     }
+    ;
+    return number;
+  }
 
-    float validateNumeric(string number){
-        float convertedNumber;
-        while(true){
-            try {
-                convertedNumber = 
-            } catch (Exception e) {
-                // TODO: handle exception
-            }
+  float validateNumber(string number) {
+    Scanner scanner = new Scanner(System.in);
+    float convertedNumber;
+    while (true) {
+      try {
+        convertedNumber = Float.parseFloat(number);
+        while (convertedNumber <= 0) {
+          System.out.println("You entered an invalid number");
+          System.out.println("the number should be a positive");
+          System.out.print("Please enter a valid number: ");
+          convertedNumber = Float.parseFloat(scanner.nextLine());
         }
+        ;
+        return convertedNumber;
+
+      } catch (Exception e) {
+        System.out.println("You entered an invalid number");
+        System.out.println("the number should be a positive");
+        System.out.print("Please enter a valid number: ");
+        number = scanner.nextLine();
+      }
     }
+  }
 }
