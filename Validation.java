8e76013de28867579dcee729fd4ad27b597bec47
ijromerob <ijromerob@gmail.com>
2025-03-15 22:57:33 -0600

    added comments to validation

--- Validation.java
+++ Validation.java
@@ -1,13 +1,14 @@
 import java.util.Scanner;
 
 public class Validation {
-
+  // common prompts when an input is not valid
   void reEnterPrompts() {
     System.out.println("You entered an invalid number");
     System.out.println("the number should be a positive");
     System.out.print("Please enter a valid number: ");
   }
 
+  // this method validates the number entered into the system.
   float validateNumber(String number) {
     Scanner scanner = new Scanner(System.in);
     float convertedNumber;
